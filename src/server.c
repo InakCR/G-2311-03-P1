8144a5d545093273b9b9428f59f373f00f493418
InakCR <inakireymundo@gmail.com>
2017-02-13 00:15:20 +0100

    Daemon

---
+++ src/server.c
@@ -0,0 +1,18 @@
+#include "../includes/lib.h"
+
+int main(int argc, char *argv[]) {
+        int socketServer;
+
+        if (daemonizar("REDES2") < 0)
+                on_error(LOG_ERR, "Error daemonizar");
+
+        socketServer = iniciar_servidor(PORT);
+
+        // Apertura del socket
+        if (socketServer < 0)
+                on_error(LOG_ERR, "Error iniciar_servidor");
+
+        //Accept and incoming connection
+        syslog(LOG_INFO, "Waiting for incoming connections...");
+        return 0;
+}
