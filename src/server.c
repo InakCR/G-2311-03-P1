c96601379dcac1e2de249ba45b35793d9edd6bb1
InakCR <inakireymundo@gmail.com>
2017-02-14 10:18:59 +0100

    Ultima

--- src/server.c
+++ src/server.c
@@ -15,8 +15,5 @@ int main(int argc, char *argv[]) {
         //Accept and incoming connection
         syslog(LOG_INFO, "Waiting for incoming connections...");
 
-
-        //SSs
-        //ff
         return 0;
 }
