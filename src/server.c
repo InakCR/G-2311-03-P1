b2e2c3e3dce09603ea4986596b2a7cf75230c2f4
kiqe00 <kiqe00@gmail.com>
2017-02-14 10:09:25 +0100

    he modificado noseq

--- src/server.c
+++ src/server.c
@@ -15,4 +15,6 @@ int main(int argc, char *argv[]) {
         //Accept and incoming connection
         syslog(LOG_INFO, "Waiting for incoming connections...");
         return 0;
+
+        //probando
 }
