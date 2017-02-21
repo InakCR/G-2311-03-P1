667ffd3623a259a44878fbadfcdff1960c24867e
InakCR <inakireymundo@gmail.com>
2017-02-21 10:16:38 +0100

    Server Eco

--- src/server.c
+++ src/server.c
@@ -4,43 +4,51 @@
 #define MSTIEMPO 30000
 
 int main(int argc, char *argv[]) {
-        int socketServer,socketClient,err,ncliente;
-        fd_set readfds;
-        struct timeval timeout;
-        pthread_t ptCliente[MAX_CLIENTS];
-        pthread_attr_t attr;
-        //Crecion de proceso Daemon
-        if (daemonizar("REDES2") < 0)
-                on_error(LOG_ERR, "Error daemonizar");
+  int socketServer, socketClient, err, ncliente, max;
+  fd_set readfds;
+  struct timeval timeout;
+  pthread_t ptCliente[MAX_CLIENTS];
+  pthread_attr_t attr;
+  // Crecion de proceso Daemon
+  if (daemonizar("REDES2") < 0)
+    on_error(LOG_ERR, "Error daemonizar");
 
-        socketServer = ini_server(PORT);
+  socketServer = ini_server(PORT);
 
-        // Apertura del socket
-        if (socketServer < 0)
-                on_error(LOG_ERR, "Error iniciar_servidor");
+  // Apertura del socket
+  if (socketServer < 0)
+    on_error(LOG_ERR, "Error iniciar_servidor");
 
-        // Accept and incoming connection
-        syslog(LOG_INFO, "Esperando conexiones de clientes");
-        ncliente = 0;
-        while (1) {
-                //Preparando select
-                FD_ZERO(&readfds);
-                FD_SET(socketServer, &readfds);
+  // Accept and incoming connection
+  syslog(LOG_INFO, "Esperando conexiones de clientes");
+  ncliente = 0;
+  max = MAX_CLIENTS;
 
-                timeout.tv_sec = STIEMPO;
-                timeout.tv_usec = MSTIEMPO;
+  while (1) {
+    // Preparando select
+    FD_ZERO(&readfds);
+    FD_SET(socketServer, &readfds);
 
-                if(select(FD_SETSIZE,&readfds, NULL, NULL,NULL))
-                        on_error(LOG_ERR,"Error en Select");
-                // Accept
-                socketClient = accept_conex(socketServer);
-                //Tiempo de espera a una peticion
-                setsockopt (socketClient, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,sizeof(timeout));
-                // hilo conexion cliente, atendemos al cliente en el hilo
-                pthread_attr_init(&attr);
-                err = pthread_create (&ptCliente[ncliente], &attr, deal_cliente,(void *) (intptr_t)socketClient);
-                if(err < 0) on_error(LOG_ERR,"Error al crear hilo cliente");
-                ncliente++;
-        }
-        return 0;
+    timeout.tv_sec = STIEMPO;
+    timeout.tv_usec = MSTIEMPO;
+
+    if (select(max + 1, &readfds, NULL, NULL, NULL) < 0)
+      on_error(LOG_ERR, "Error en Select");
+    syslog(LOG_INFO, "Nuevo Cliente");
+    // Accept
+    socketClient = accept_conex(socketServer);
+    // Tiempo de espera a una peticion
+    setsockopt(socketClient, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,
+               sizeof(timeout));
+    setsockopt(socketClient, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout,
+               sizeof(timeout));
+    // hilo conexion cliente, atendemos al cliente en el hilo
+    pthread_attr_init(&attr);
+    err = pthread_create(&ptCliente[ncliente], &attr, deal_cliente,
+                         (void *)(intptr_t)socketClient);
+    if (err < 0)
+      on_error(LOG_ERR, "Error al crear hilo cliente");
+    ncliente++;
+  }
+  return 0;
 }
--- srclib/lib.c
+++ srclib/lib.c
@@ -1,110 +1,115 @@
 
 #include "../includes/lib.h"
 
-void on_error(int logPri,char* err){
-        syslog(logPri,"%s",err);
-        exit(1);
+void on_error(int logPri, char *err) {
+  syslog(logPri, "%s", err);
+  exit(1);
 }
-int ini_server(int port){
-        struct sockaddr_in server;
-        int sockserv,opt_val = 1;
+int ini_server(int port) {
+  struct sockaddr_in server;
+  int sockserv, opt_val = 1;
 
-        syslog(LOG_INFO, "Creating socketServer");
+  syslog(LOG_INFO, "Creating socketServer");
 
-        sockserv = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockserv < 0)
-                on_error(LOG_ERR,"Could not create socket\n");
+  sockserv = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockserv < 0)
+    on_error(LOG_ERR, "Could not create socket\n");
 
-        server.sin_family = AF_INET;
-        server.sin_port = htons(port);
-        server.sin_addr.s_addr = htonl(INADDR_ANY);
+  server.sin_family = AF_INET;
+  server.sin_port = htons(port);
+  server.sin_addr.s_addr = htonl(INADDR_ANY);
 
-        if(setsockopt(sockserv, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof opt_val) < 0)
-                on_error(LOG_ERR,"Error setsockopt\n");
-        if(bind(sockserv, (struct sockaddr *) &server, sizeof(server)) < 0 )
-                on_error(LOG_ERR,"Could not bind socket\n");
+  if (setsockopt(sockserv, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof opt_val) <
+      0)
+    on_error(LOG_ERR, "Error setsockopt\n");
+  if (bind(sockserv, (struct sockaddr *)&server, sizeof(server)) < 0)
+    on_error(LOG_ERR, "Could not bind socket\n");
 
-        /*Procedemos a escuchar llamadas de clientes*/
-        if(listen(sockserv, MAX_CLIENTS) < 0) {
-                close(sockserv);
-                on_error(LOG_ERR,"Error listening clients");
-        }
+  /*Procedemos a escuchar llamadas de clientes*/
+  if (listen(sockserv, MAX_CLIENTS) < 0) {
+    close(sockserv);
+    on_error(LOG_ERR, "Error listening clients");
+  }
 
-        syslog(LOG_INFO, "Server is listening on %d",port);
+  syslog(LOG_INFO, "Server is listening on %d", port);
 
-        return sockserv;
+  return sockserv;
 }
-int daemonizar(char* service) {
+int daemonizar(char *service) {
 
-        int pid, i, fd;
+  int pid, i, fd;
 
-        pid = fork();
-        if( pid < 0 ) exit(1);
-        if( pid > 0 ) exit(0);
+  pid = fork();
+  if (pid < 0)
+    exit(1);
+  if (pid > 0)
+    exit(0);
 
-        // resetting File Creation Mask
-        umask(0);
-        setlogmask (LOG_UPTO (LOG_INFO));
-        openlog(service, LOG_PID, LOG_LOCAL3);
-        syslog(LOG_INFO, "Initiating_new_server.");
+  // resetting File Creation Mask
+  umask(0);
+  setlogmask(LOG_UPTO(LOG_INFO));
+  openlog(service, LOG_PID, LOG_LOCAL3);
+  syslog(LOG_INFO, "Initiating_new_server.");
 
-        if(setsid()<0)
-                on_error(LOG_ERR,"Error_creating_a_new_SID_for_the_child_process.");
+  if (setsid() < 0)
+    on_error(LOG_ERR, "Error_creating_a_new_SID_for_the_child_process.");
 
-        // Directorio raiz
-        if((chdir("/")) < 0)
-                on_error(LOG_ERR,"Error_changing_the_current_working_directory_=_\"/\"");
+  // Directorio raiz
+  if ((chdir("/")) < 0)
+    on_error(LOG_ERR, "Error_changing_the_current_working_directory_=_\"/\"");
 
-        syslog(LOG_INFO, "Closing_standard_file_descriptors");
+  syslog(LOG_INFO, "Closing_standard_file_descriptors");
 
-        // close all descriptors
-        for (i = getdtablesize(); i>=0; --i)
-                close(i);
+  // close all descriptors
+  for (i = getdtablesize(); i >= 0; --i)
+    close(i);
 
-        fd = open("dev/null", O_RDWR, 0);
+  fd = open("dev/null", O_RDWR, 0);
 
-        if (fd != 1) {
+  if (fd != 1) {
 
-                dup2(fd, STDIN_FILENO);
-                dup2(fd, STDOUT_FILENO);
-                dup2(fd, STDERR_FILENO);
+    dup2(fd, STDIN_FILENO);
+    dup2(fd, STDOUT_FILENO);
+    dup2(fd, STDERR_FILENO);
 
-                if (fd > 2)
-                        close(fd);
+    if (fd > 2)
+      close(fd);
+  }
 
-        }
+  syslog(LOG_INFO, "Daemon creado");
+  closelog();
 
-        syslog(LOG_INFO, "Closing_server.");
-        closelog();
-
-        return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
 int accept_conex(int sock) {
 
-        int length, socketClient;
-        struct sockaddr_in client;
+  int length, socketClient;
+  struct sockaddr_in client;
 
-        length = sizeof(struct sockaddr_in);
+  length = sizeof(struct sockaddr_in);
 
-        // Accept connection from an incoming client
-        if ((socketClient = accept(sock, (struct sockaddr *)&client, (socklen_t*)&length)))
-                on_error(LOG_ERR,"Error accept conexion cliente");
+  // Accept connection from an incoming client
+  if ((socketClient =
+           accept(sock, (struct sockaddr *)&client, (socklen_t *)&length)) < 0)
+    on_error(LOG_ERR, "Error accept conexion cliente");
 
-
-        return socketClient;
-
+  return socketClient;
 }
-void* deal_cliente(void* sock){
-        int read,err,socket;
-        char buf[BUFFER_SIZE];
-        socket =*((int*)(&sock));
-        while(1) {
-                int read = recv(socket, buf, BUFFER_SIZE, 0);
+void *deal_cliente(void *sock) {
+  int read, err, socket;
+  char buf[BUFFER_SIZE];
+  socket = *((int *)(&sock));
+  while (1) {
+    int read = recv(socket, buf, BUFFER_SIZE, 0);
 
-                if (!read) break; // socket cerrado
-                if (read < 0) on_error(LOG_ERR,"Client read failed");
+    if (read < 0) { // Socket cerrado
+      syslog(LOG_INFO, "Client close timeout");
+      close(socket);
+      break;
+    }
 
-                err = send(socket, buf, read, 0);
-                if (err < 0) on_error(LOG_ERR,"Client write failed");
-        }
+    err = send(socket, buf, read, 0);
+    if (err < 0)
+      on_error(LOG_ERR, "Client write failed");
+  }
 }
